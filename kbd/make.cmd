737eeaaebd2924398fcd89df861754d2079a9838
prokushev <yuri.prokushev@gmail.com>
2021-11-15 20:18:13 +0700

    build system improvements

--- kbd/make.cmd
+++ kbd/make.cmd
@@ -1,34 +1,6 @@
 @echo off
 
 echo Building Keyboard Subsystem
-if exist kbd.lib del kbd.lib
-if exist *.obj del *.obj
-if exist *.lst del *.lst
-if exist *.err del *.err
-if exist *.bak del *.bak
-
-rem Keyboard subsystem
-jwasm -q kbdroute.asm
-jwasm -q kbdcharin.asm
-jwasm -q kbdpeek.asm
-jwasm -q kbdflushbuffer.asm
-jwasm -q kbdgetstatus.asm
-jwasm -q kbdsetstatus.asm
-jwasm -q kbdstringin.asm
-jwasm -q kbdopen.asm
-jwasm -q kbdclose.asm
-jwasm -q kbdgetfocus.asm
-jwasm -q kbdfreefocus.asm
-jwasm -q kbdgetcp.asm
-jwasm -q kbdsetcp.asm
-jwasm -q kbdxlate.asm
-jwasm -q kbdsetcustxt.asm
-jwasm -q kbdgethwid.asm
-
-rem Kbd Subsystem
-wlib -q -fo kbd.lib +kbdroute.obj +kbdcharin.obj +kbdpeek.obj +kbdflushbuffer.obj +kbdgetstatus.obj +kbdsetstatus.obj
-wlib -q -fo kbd.lib +kbdstringin.obj +kbdopen.obj +kbdclose.obj +kbdgetfocus.obj +kbdfreefocus.obj +kbdgetcp.obj
-wlib -q -fo kbd.lib +kbdsetcp.obj +kbdxlate.obj +kbdsetcustxt.obj +kbdgethwid.obj
-
+wmake -h
 echo Keyboard Subsystem build finished
 
--- makefile
+++ makefile
@@ -2,8 +2,8 @@ ALL: fapi.lib
 
 OBJS=globalshared.obj helpers.obj baddynlink.obj ginfoseg.obj linfoseg.obj globalvars.obj
 
-fapi.lib: lib/dos.lib lib/mem.lib lib/fm.lib lib/ioctl.lib vio/vio.lib bvs/bvs.lib kbd/kbd.lib lib/bks.lib mou/mou.lib bms/bms.lib $(OBJS)
-  wlib -q -fo fapi.lib +lib/dos.lib +lib/mem.lib +lib/fm.lib +lib/ioctl.lib +vio/vio.lib +bvs/bvs.lib +kbd/kbd.lib +lib/bks.lib +mou/mou.lib +bms/bms.lib $(OBJS)
+fapi.lib: lib/dos.lib lib/mem.lib lib/fm.lib lib/ioctl.lib lib/vio.lib lib/bvs.lib lib/kbd.lib lib/bks.lib lib/mou.lib lib/bms.lib $(OBJS)
+  wlib -q -fo fapi.lib +lib/dos.lib +lib/mem.lib +lib/fm.lib +lib/ioctl.lib +lib/vio.lib +lib/bvs.lib +lib/kbd.lib +lib/bks.lib +lib/mou.lib +lib/bms.lib $(OBJS)
 
 .asm.obj:
 	@jwasm.exe -q -Iinclude $*.asm
