57a52f40af251017524d31f8d97843dad0adb00a
prokushev <yuri.prokushev@gmail.com>
2022-02-02 16:19:00 +0700

    MouShellInit stub (mostly finished)

--- mou/MouShellInit.asm
+++ mou/MouShellInit.asm
@@ -17,10 +17,27 @@
 .8086
 		; Helpers
 		INCLUDE	HELPERS.INC
+		INCLUDE	BSEDOS.INC
+		INCLUDE	BSEERR.INC
 
+; Global Data
 _DATA		SEGMENT BYTE PUBLIC 'DATA' USE16
+
 CharStr		DB 'MouShellInit'
 CharStr_size equ ($-charstr)
+
+MOUSEFLAG	DW	0					; Is mouse device driver presented
+MOUSEDD		DB	'MOUSE$', 0			; Mouse device driver name
+
+;-- move to stack --
+MOUSEH		DW	?					; Mouse device driver handle
+MOUSEA		DW	?					; ActionTaken on mouse device driver open
+GBL			DD	?
+LCL			DD	?
+;-- move to stack --
+
+SHELL_PID	DW	?					; PID of session manager/shell
+
 _DATA		ENDS
 
 EXTERN	VioWrtTTY: FAR
@@ -30,6 +47,18 @@ _TEXT		SEGMENT BYTE PUBLIC 'CODE' USE16
 		PUBLIC	MOUSHELLINIT
 MOUSHELLINIT	PROC FAR
 		XOR		AX, AX				; RC=NO_ERROR
+		@DosOpen	MOUSEDD, MOUSEH, MOUSEA, 0, 0, 1, 42h, 0
+		CMP		AX, 0
+		MOV		AX, ERROR_MOUSE_NO_DEVICE	;ERR
+		JNE		BAD
+		MOV		AX, SEG _DATA
+		MOV		ES, AX
+		@DosClose	ES:MOUSEH
+		MOV		ES:MOUSEFLAG, 1
+		@DosGetInfoSeg	GBL, LCL
+		XOR		AX, AX
+BAD:
+
 		@PUSHA
 		MOV		AX, SEG CHARSTR
 		PUSH	AX
@@ -41,6 +70,7 @@ MOUSHELLINIT	PROC FAR
 		PUSH	AX
 		CALL	VioWrtTTY
 		@POPA
+
 		RETF
 MOUSHELLINIT		ENDP
 
