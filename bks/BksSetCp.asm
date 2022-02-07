73876cab8cec1c9338e259970631af6ddba9b5d7
prokushev <yuri.prokushev@gmail.com>
2022-02-07 15:26:14 +0700

    kbdsetcp added

--- bks/BksSetCp.asm
+++ bks/BksSetCp.asm
@@ -13,6 +13,16 @@
 ;
 ;   @author Yuri Prokushev (yuri.prokushev@gmail.com)
 ;
+;INT 2F - DOS 3.3+ KEYB.COM - SET KEYBOARD CODE PAGE
+;
+;	AX = AD81h
+;	BX = code page (see #01757 at INT 21/AX=6601h)
+;Return: CF set on error
+;	    AX = 0001h (code page not available)
+;	CF clear if successful
+;Notes:	called by DISPLAY.SYS
+;	this function was undocumented prior to the release of DOS 5.0
+;SeeAlso: AX=AD80h,AX=AD82h
 ;*/
 
 .8086
@@ -25,7 +35,7 @@ _TEXT		SEGMENT BYTE PUBLIC 'CODE' USE16
 
 		@BKSPROLOG	BKSSETCP
 KBDHANDLE	DW	?		;KEYBOARD HANDLE
-CODEPAGEID	DD	?		;
+CODEPAGEID	DW	?		;
 RESERVED	DD	?		;
 		@BKSSTART	BKSSETCP
 		MOV	AX,ERROR_KBD_PARAMETER
@@ -34,7 +44,10 @@ RESERVED	DD	?		;
 		JNZ	EXIT
 		CMP	BX, WORD PTR [DS:BP].ARGS.RESERVED+2
 		JNZ	EXIT
-; code here
+
+		MOV	BX, WORD PTR [DS:BP].ARGS.CODEPAGEID
+		MOV	AX, 0AD81h
+		INT	2FH
 EXIT:
 		@BKSEPILOG	BKSSETCP
 
