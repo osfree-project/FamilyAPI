db9c92eaa2ed875a48ad58bfb2c24dfa9a728f66
prokushev <yuri.prokushev@gmail.com>
2024-06-20 16:03:04 +0700

    DosScanEnv initial implementation, DosGetMessage proto fix, initial implementation.

--- dos/DosScanEnv.asm
+++ dos/DosScanEnv.asm
@@ -5,7 +5,7 @@
 ;
 ;   @brief DosScanEnv DOS wrapper
 ;
-;   (c) osFree Project 2022, <http://www.osFree.org>
+;   (c) osFree Project 2022,2024 <http://www.osfree.org>
 ;   for licence see licence.txt in root directory, or project website
 ;
 ;   This is Family API implementation for DOS, used with BIND tools
@@ -14,6 +14,9 @@
 ;   @author Yuri Prokushev (yuri.prokushev@gmail.com)
 ;
 ;
+; 0 NO_ERROR
+; ERROR_INVALID_PARAMETER
+; ERROR_ENVVAR_NOT_FOUND
 ;
 ;*/
 
@@ -21,13 +24,102 @@
 
 		; Helpers
 		INCLUDE	helpers.inc
+		INCLUDE	bseerr.inc
 
 _TEXT		SEGMENT BYTE PUBLIC 'CODE' USE16
 
+		; External functions
+EXTERN		DosGetEnv: FAR
+
 		@PROLOG	DOSSCANENV
+RESULTPOINTER	DD	?
+ENVVARNAME		DD	?
+@LOCALW			CMD
+@LOCALW			ENV
 		@START	DOSSCANENV
 
-		XOR	AX, AX
+		; Check arguments not NULL
+		MOV		AX, ERROR_INVALID_PARAMETER
+		XOR		BX, BX
+
+		CMP		BX, WORD PTR [DS:BP].ARGS.RESULTPOINTER
+		JNE		RESULTPOINTEROK
+		CMP		BX, WORD PTR [DS:BP].ARGS.RESULTPOINTER+2
+		JE		EXIT
+RESULTPOINTEROK:
+
+		CMP		BX, WORD PTR [DS:BP].ARGS.ENVVARNAME
+		JNE		ENVVARNAMEOK
+		CMP		BX, WORD PTR [DS:BP].ARGS.ENVVARNAME+2
+		JE		EXIT
+ENVVARNAMEOK:
+
+		; Get environment
+		PUSH	SS
+		LEA		AX, ENV
+		PUSH	AX
+
+		PUSH	SS
+		LEA		AX, CMD
+		PUSH	AX
+
+		CALL	DosGetEnv
+
+		; Exit if error
+		OR		AX, AX
+		JNZ		EXIT
+
+		; Calc length of searched varname
+		LES		DI, [DS:BP].ARGS.ENVVARNAME
+		MOV		CX, 0FFFFH
+		;XOR	AL, AL		; Zero here already
+		REPNE	SCASB
+		NOT		CX
+		DEC		CX
+		MOV		DX, CX
+
+		; Exit if zero length
+		MOV		AX, ERROR_ENVVAR_NOT_FOUND
+		OR		CX, CX
+		JZ		EXIT
+
+		; ES:DI - environment
+		MOV		ES, ENV
+		XOR		DI, DI
+		; DS:SI - searched environment variable
+		LDS		SI, [DS:BP].ARGS.ENVVARNAME
+
+SEARCH:
+		MOV		SI, WORD PTR [DS:BP].ARGS.ENVVARNAME
+		MOV		CX, DX
+		REPE	CMPSB
+		JNZ		NOTFOUND
+
+		; Check is found
+		CMP		BYTE PTR [ES:DI],'='
+		JZ		FOUND
+
+NOTFOUND:
+		; Skip to next variable
+		DEC		DI
+		MOV		CX, 0FFFFH
+		XOR		AL, AL
+		REPNE	SCASB
+		
+		; Exit if end of environment
+		MOV		AX, ERROR_ENVVAR_NOT_FOUND
+		CMP		BYTE PTR ES:[DI],0
+		JZ		EXIT
+		JMP		SEARCH
+
+		; Store result and exit
+FOUND:
+		INC		DI
+		LDS		SI, [DS:BP].ARGS.RESULTPOINTER
+		MOV		[SI], DI
+		MOV		[SI + 2], ES
+		XOR		AX, AX
+
 EXIT:
 		@EPILOG	DOSSCANENV
 
--- nls/DosGetMessage.asm
+++ nls/DosGetMessage.asm
@@ -14,7 +14,7 @@
 ;   @author Yuri Prokushev (yuri.prokushev@gmail.com)
 ;
 ;
-;    DosGetMessage (IvTable, IvCount, DataArea, DataLength, MsgNumber, FileName, MsgLength)
+;    DosTrueGetMessage (IvTable, IvCount, DataArea, DataLength, MsgNumber, FileName, MsgLength, MsgSeg)
 ;
 ;    IvTable (PCHAR FAR *) - input : Address of a list of double-word pointers. Each pointer points to an ASCIIZ or null-terminated DBCS string (variable insertion text). 0 to 9 strings can be present.
 ;    IvCount (USHORT) - input:Count of variable insertion text strings is 0-9. If IvCount is 0, IvTable is ignored.
@@ -23,6 +23,7 @@
 ;    MsgNumber (USHORT) - input:Requested message number.
 ;    FileName (PSZ) - input:Address of the optional drive, path, and filename of the file where the message can be found. If messages are bound to the .EXE file using MSGBIND utility, then filename is the name of the message file from which the messages are extracted.
 ;    MsgLength (PUSHORT) - output:Address of the length, in bytes, of the message.
+;    MsgSeg (VOID FAR *) - input: Pointer to binded Messages segment.
 ;
 ;    0 NO_ERROR
 ;    2 ERROR_FILE_NOT_FOUND
@@ -45,6 +46,9 @@
 
 _TEXT		SEGMENT BYTE PUBLIC 'CODE' USE16
 
+		; External functions
+EXTERN		DosScanEnv: FAR
+		
 		; Extracted messages
 EXTERN		_TXT_MSG_MR_CANT_FORMAT: FAR
 EXTERN		_TXT_MSG_MR_NOT_FOUND: FAR
@@ -52,8 +56,8 @@ EXTERN		_TXT_MSG_MR_READ_ERROR: FAR
 EXTERN		_TXT_MSG_MR_IVCOUNT_ERROR: FAR
 EXTERN		_TXT_MSG_MR_UN_PERFORM: FAR
 
-
 		@PROLOG	DOSTRUEGETMESSAGE
+MSGSEG		DD	?
 MSGLENGTH	DD	?
 FILENAME	DD	?
 MSGNUMBER	DW	?
@@ -61,7 +65,6 @@ DATALENGTH	DW	?
 DATAAREA	DD	?
 IVCOUNT		DW	?
 IVTABLE		DD	?
-MSGSEG		DW	?
 		@START	DOSTRUEGETMESSAGE
 
 		; Save instance segment
@@ -69,7 +72,9 @@ MSGSEG		DW	?
 		; Set direction
 		CLD
 
-		; Parameters check block without error message copied to message buffer
+		;@DispCh '1'
+
+		; **Parameters check block without error message copied to message buffer**
 		
 		; Check message buffer ponter
 		MOV	AX,ERROR_INVALID_PARAMETER
@@ -77,62 +82,81 @@ MSGSEG		DW	?
 
 		CMP	BX, WORD PTR [DS:BP].ARGS.DATAAREA
 		JNE	DATAAREAOK
+		;@DispCh '2'
 		CMP	BX, WORD PTR [DS:BP].ARGS.DATAAREA+2
 		JE	EXIT
 DATAAREAOK:
+
+		;@DispCh '3'
 
 		; Check buffer size
 		CMP	BX, WORD PTR [DS:BP].ARGS.DATALENGTH
 		JE	EXIT
 
+		;@DispCh '4'
+
 		; Check filename pointer
 		CMP	BX, WORD PTR [DS:BP].ARGS.FILENAME
 		JNE	FILENAMEOK
+		@DispCh '5'
 		CMP	BX, WORD PTR [DS:BP].ARGS.FILENAME+2
-		JE	EXIT
+		JE	EXIT		; @todo Skip file, only segment search
 FILENAMEOK:
+		;@DispCh '6'
 
 		; Parameters check block with error message copied to message buffer
 
 		; Set destination buffer
 		LES	DI, [DS:BP].ARGS.DATAAREA
 
+		; @todo Search message segment
+
+		; Search message file
+
+		; First try default drive root directory
+		;PUSH	DWORD EnvVarName    ; COMSPEC
+		;PUSH	DWORD ResultPointer	; Value
+		;CALL	DosScanEnv
+		; Second open in current directory (APPEND allow search in anothe paths)
+		
+		MOV	CX, [DS:BP].ARGS.DATALENGTH
+
 		; Check IVCOUNT value is less 9
 		MOV	AX, ERROR_MR_INV_IVCOUNT
 		CMP	WORD PTR [DS:BP].ARGS.IVCOUNT, 9
 		JG	BADIVCOUNTEXIT
 
-		; Search message segment
-		; Search message file
-		
 		; Copy string and insert IVT items
-		PUSH	CS
-		POP	DS
-		MOV	SI, OFFSET MSGBUF
-		
+
+OutMsg:		
 		; Output message size counter
 		XOR	BX,BX
 
 Continue:	
 		INC	BX
-		MOV	DS, CS:[InstanceSeg]
-		CMP	BX, [DS:BP].ARGS.DATALENGTH
+		CMP	BX, CX			; ARGS.DATALENGTH
 		JG	BUFOUTEXIT
-		PUSH	CS
-		POP	DS
 
 		CMP	BYTE PTR [DS:SI], '%'
 		JE	InsertIVItem
-	
-		;MOV	AL, BYTE PTR [DS:SI]
-		;@DispCh AL
-		CMP	BYTE PTR [DS:SI], 0
+
+
+		push ax
+		MOV	AL, BYTE PTR [DS:SI]
+		@DispCh AL
+		pop ax
+		
 		MOVSB
+
+		CMP	BYTE PTR [DS:SI], 0
 		JE	EXIT
 
 		JMP	Continue
 
 InsertIVItem:	
+		push ax
+		@DispCh 'X'
+		pop ax
 		; Check is IVT present
 		CMP	[DS:BP].ARGS.IVCOUNT, 0
 		JE	Continue
@@ -152,42 +176,74 @@ InsertIVItem:
 		POP	SI
 		JMP	Continue
 
-MSGBUF		DB	'test', 10,13, 0
 InstanceSeg	DW	0			; Instance segment
 
 		; Copy extracted messages to message buffer
 BADMSGID:
-		LDS	SI, DWORD PTR _TXT_MSG_MR_CANT_FORMAT
-		JMP	Continue
+		PUSH CS
+		POP DS
+		MOV	SI, OFFSET _TXT_MSG_MR_CANT_FORMAT
+		JMP	OutMsg
 
 BADMSGFORMAT:
-		LDS	SI, DWORD PTR _TXT_MSG_MR_READ_ERROR
-		JMP	Continue
+		PUSH CS
+		POP DS
+		MOV	SI, OFFSET _TXT_MSG_MR_READ_ERROR
+		JMP	OutMsg
 
 BADIVCOUNTEXIT:
-		LDS	SI, DWORD PTR _TXT_MSG_MR_IVCOUNT_ERROR
-		JMP	Continue
+		PUSH CS
+		POP DS
+		MOV	SI, OFFSET _TXT_MSG_MR_IVCOUNT_ERROR
+		JMP	OutMsg
 
 BUFOUTEXIT:
 		MOV	AX, ERROR_MR_MSG_TOO_LONG
+		push ax
+		@DispCh 'Y'
+		pop ax
 		JMP	EXIT
 
 OKEXIT:		
 		XOR	AX, AX
 EXIT:		
+		; Restore instance segment
+		MOV	DS, CS:[InstanceSeg]
 		@EPILOG	DOSTRUEGETMESSAGE
+
 
 _TEXT		ENDS
 
-_MKMSGSEG	SEGMENT
+_MKMSGSEG	SEGMENT BYTE PUBLIC 'CODE' USE16
 		DB        0FFH, 'MKMSGSEG', 0, 1, 0, 0, 0, 0, 0
+
+
+;    DosGetMessage (IvTable, IvCount, DataArea, DataLength, MsgNumber, FileName, MsgLength)
+;
+;    IvTable (PCHAR FAR *) - input : Address of a list of double-word pointers. Each pointer points to an ASCIIZ or null-terminated DBCS string (variable insertion text). 0 to 9 strings can be present.
+;    IvCount (USHORT) - input:Count of variable insertion text strings is 0-9. If IvCount is 0, IvTable is ignored.
+;    DataArea (PCHAR) - output:Address of the requested message. If the message is too long to fit in the caller's buffer, as much of the message text is returned as possible, with the appropriate error return code.
+;    DataLength (USHORT) - input:Length, in bytes, of the user's storage area.
+;    MsgNumber (USHORT) - input:Requested message number.
+;    FileName (PSZ) - input:Address of the optional drive, path, and filename of the file where the message can be found. If messages are bound to the .EXE file using MSGBIND utility, then filename is the name of the message file from which the messages are extracted.
+;    MsgLength (PUSHORT) - output:Address of the length, in bytes, of the message.
+;
+;    0 NO_ERROR
+;    2 ERROR_FILE_NOT_FOUND
+;    206 ERROR_FILENAME_EXCED_RANGE
+;    316 ERROR_MR_MSG_TOO_LONG
+;    317 ERROR_MR_MID_NOT_FOUND
+;    318 ERROR_MR_UN_ACC_MSGF
+;    319 ERROR_MR_INV_MSFG_FORMAT
+;    320 ERROR_MR_INV_IVCOUNT
+;    321 ERROR_MR_UN_PERFORM
 
 		PUBLIC	DOSGETMESSAGE
 DOSGETMESSAGE:
-		XOR       AX,AX
+		XOR       AX,AX			; Message segment
 		PUSH      AX
 		PUSH      CS
-		PUSH      BP
+		PUSH      BP			; Move exchange return address and Message segment
 		MOV       BP,SP
 		XCHG      AX,WORD PTR 6H[BP]
 		XCHG      AX,WORD PTR 2H[BP]
